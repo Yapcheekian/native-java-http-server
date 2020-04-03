4d8b0448597ca86e5e59a83da265540effc4ff56
Yap Chee Kian <iforte_intellige.cheekian@daimler.com>
2020-04-03 19:51:50 +0800

    http server

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
-    public static void main(String[] args) {
-      // write your code here
+    public static void main(String[] args) throws IOException {
+        System.out.println("hello");
+        final ServerSocket serverSocket = new ServerSocket(8080);
+        while (true) {
+            final Socket socket = serverSocket.accept();
+            final OutputStream outputStream = socket.getOutputStream();
+            final PrintWriter printWriter = new PrintWriter(outputStream);
+            System.out.println("Connected to" + socket.getRemoteSocketAddress());
+            printWriter.println("HTTP/1.0 200 OK");
+            printWriter.println("Content-Length: 12");
+            printWriter.println("");
+            printWriter.println("Hello World!");
+            printWriter.flush();
+            System.out.println("Sent.");
+            socket.close();
+        }
     }
 }
