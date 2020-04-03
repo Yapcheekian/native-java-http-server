ac89dfa367f52644d7758e6496e3f132c0b68415
Yap Chee Kian <iforte_intellige.cheekian@daimler.com>
2020-04-03 21:25:56 +0800

    two way communication between client and server

---
+++ src/com/company/server/Server.java
@@ -0,0 +1,25 @@
+package com.company.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+  public static void main(String[] args) throws IOException {
+    ServerSocket ss = new ServerSocket(8080);
+    Socket s = ss.accept();
+
+    InputStreamReader in = new InputStreamReader(s.getInputStream());
+    BufferedReader bf = new BufferedReader(in);
+
+    String str = bf.readLine();
+    System.out.println("client" + str);
+
+    PrintWriter pr = new PrintWriter(s.getOutputStream());
+    pr.println("yes it is");
+    pr.flush();
+  }
+}
